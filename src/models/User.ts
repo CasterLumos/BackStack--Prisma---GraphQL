49e4d157fde3f5702f1aea2a36fd223f2746e18b
Caster Lumos <48294478+CasterLumos@users.noreply.github.com>
2022-04-18 06:19:48 -0300

    🔨➕Add Client User JWT

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import { Field, ID, ObjectType } from "type-graphql"
+import { Field, ID, InputType, ObjectType } from "type-graphql"
 
 @ObjectType()
 export class User {
@@ -6,4 +6,31 @@ export class User {
     id: string;
     @Field()
     name: string;
+    @Field()
+    address: string;
+    @Field()
+    city: string;
+    @Field()
+    phone: string;
+    @Field()
+    mail: string;
+    @Field()
+    password: string;
+    @Field()
+    token: string;
+}
+@InputType()
+export class UserData {
+    @Field()
+    name: string;
+    @Field()
+    address: string;
+    @Field()
+    city: string;
+    @Field()
+    phone: string;
+    @Field()
+    mail: string;
+    @Field()
+    password: string;
 }
--- src/resolvers/userResolver.ts
+++ src/resolvers/userResolver.ts
@@ -1,19 +1,33 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
-import { User } from "../models/User";
+import pkg from "jsonwebtoken";
+import { User, UserData } from "../models/User";
 import crypto from "crypto";
+import * as dotenv from "dotenv";
+import bcrypt from 'bcryptjs';
+dotenv.config();
+const { sign } = pkg;
+
+function generateToken(params = {}) {
+    const IV: string = (process.env.IV as string);
+    return sign(params, IV ,{expiresIn:86400,})
+}
+
 
 @Resolver()
 export class UserResolver {
     private data: User[] =[];
+    @Mutation(() => User)
+    async createUser(@Arg('dataUser') dataUser: UserData) {
+        const {name, address, city,phone,mail, password} = dataUser
+        const id = crypto.randomUUID()
+        const token = generateToken({ id })
+        const ENCRYPTION_KEY: string = (process.env.ENCRYPTION_KEY as string);
+        const user = {id, name, address, city,phone,mail,token, password: await bcrypt.hashSync(password, 12)}
+        this.data.push(user)
+        return user
+    }
     @Query(() =>[User])
     async users() {
         return this.data;
-    }
-    @Mutation(() => User)
-    async createUser(
-        @Arg('name') name: string) {
-        const user = {id: crypto.randomUUID(), name}
-        this.data.push(user)
-        return user
     }
 } 
